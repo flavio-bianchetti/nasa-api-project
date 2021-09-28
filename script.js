7751ee4f247209b2ad1669755c6200267d040f39
CaioMagalhaesPinheiro <caiomgp22@gmail.com>
2021-09-28 11:37:02 -0300

    Adicionando div do planetas e pesquisa de imagem

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const url = 'https://images-api.nasa.gov/search?q='
 let whatSearch;
 
-const buttonSearch = document.querySelector('.search-button');
-buttonSearch.addEventListener('click', searchImage)
+function selectPlanet(event) {
+  const planet = event.target.innerText;
+  whatSearch = planet;
+  getAPI();
+}
 
 function searchImage() {
-  const inputSearch = document.querySelector('.search').value;
-  whatSearch = inputSearch;
-  getAPI();
+  const sectionPlanets = document.querySelector('.section-planets');
+  sectionPlanets.addEventListener('click', selectPlanet);
 }
 
 function getAPI () {
   const body = document.querySelector('body');
   const img = document.createElement('img');
   fetch(`${url}${whatSearch}`)
-    .then((response) => response.json())
-    .then((element) => img.src = element.collection.items[0].links[0].href);
+  .then((response) => response.json())
+  .then((element) => img.src = element.collection.items[0].links[0].href);
   body.appendChild(img)
 }
 
+window.onload = () => {
+  searchImage();
+}
